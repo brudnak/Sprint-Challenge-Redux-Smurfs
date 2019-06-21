5c67c3d9a079fd3d4fc83cd45d3268705be8efca
Andrew Brudnak <brudnak@icloud.com>
2019-06-21 11:33:27 -0700

    Everything linked up, MVP 🏁

--- smurfs/src/components/App.js
+++ smurfs/src/components/App.js
@@ -1,22 +1,26 @@
-import React, { Component } from 'react';
-import './App.css';
-/*
- to wire this component up you're going to need a few things.
- I'll let you do this part on your own. 
- Just remember, `how do I `connect` my components to redux?`
- `How do I ensure that my component links the state to props?`
- */
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <h1>SMURFS! 2.0 W/ Redux</h1>
-        <div>Welcome to your Redux version of Smurfs!</div>
-        <div>Start inside of your `src/index.js` file!</div>
-        <div>Have fun!</div>
-      </div>
-    );
-  }
-}
+import React, { Fragment } from 'react';
+import { connect } from 'react-redux';
 
-export default App;
+import SmurfForm from '../components/SmurfForm';
+import Smurfs from '../components/Smurfs';
+import GlobalStyle from './GlobalStyle';
+
+const App = props => {
+  return (
+    <Fragment>
+      <GlobalStyle />
+      <SmurfForm />
+      <Smurfs smurfs={props.smurfs} />
+    </Fragment>
+  );
+};
+const mapStateToProps = state => ({
+  smurfs: state.smurfs,
+  fetchingSmurfs: state.fetchingSmurfs,
+  error: state.error
+});
+
+export default connect(
+  mapStateToProps,
+  {}
+)(App);
--- smurfs/src/reducers/index.js
+++ smurfs/src/reducers/index.js
@@ -21,3 +21,35 @@
   There is no need for 'combineReducers' in this project.
   Components can then read your store as, `state` and not `state.fooReducer`.
 */
+
+import { FETCHING, SUCCESS, FAILURE } from '../actions';
+
+const initialState = {
+  smurfs: [],
+  fetchingSmurfs: false,
+  error: null
+};
+
+export const reducer = (state = initialState, action) => {
+  switch (action.type) {
+    case FETCHING:
+      return {
+        ...state,
+        fetchingSmurfs: false
+      };
+    case SUCCESS:
+      return {
+        ...state,
+        fetchingSmurfs: false,
+        smurfs: action.payload
+      };
+    case FAILURE:
+      return {
+        ...state,
+        fetchingSmurfs: false,
+        error: action.payload
+      };
+    default:
+      return state;
+  }
+};
